c681bc7e8b56e0e9500aaeb2b05410ae292a1b2e
leoncheng57 <57leonardo@gmail.com>
2014-09-19 08:38:24 -0400

    sdf

---
+++ Greeter.java
@@ -0,0 +1,19 @@
+/*
+g1.greetPerson("Tommy")
+returns->Whatever the greeting is Tommy
+ex.Hello Tommy
+ex.Sup Tommy
+ */
+
+public class Greeter {
+
+    private String greeting;
+    public String greetPerson(person){
+	return "Hello " + person;
+    }
+    
+/*
+g1.loudGreet()
+ex.hello->HELLO
+ */
+}

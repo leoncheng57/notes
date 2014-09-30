d2aa3ac5fd6187fc65706adcb3ea4a548943b6ac
leoncheng57 <57leonardo@gmail.com>
2014-09-30 08:27:41 -0400

    added today's notes

---
+++ 2014-09-30/Driver.java
@@ -0,0 +1,11 @@
+public class Driver {
+    public static void main(String[] args){
+	int n = 9;
+	//System.out.println(n);
+	while (n >= 0) {
+	    System.out.println(n);
+	    //n = n - 1;
+	    n -= 1;
+	}
+    }
+}

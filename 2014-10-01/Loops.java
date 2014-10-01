aa4d9ba78fc86c29cd881ecac4a819bcf623bbb4
leoncheng57 <57leonardo@gmail.com>
2014-10-01 18:25:36 -0400

    improved notes

--- 2014-10-01/Loops.java
+++ 2014-10-01/Loops.java
@@ -7,15 +7,18 @@ public class Loops {
 	String ret = "";
 	while (r>0) {
 	    int i = 0;//i will increment through the columns, I am not using c because I need to conserve the value of c to use for all of the rows
-	    while (i<c) {
+	    //this while loop below prints out one row with a number of stars
+	    while (i<c) { 
 		ret=ret+"*";
 		i = i+1;
 	    }
-	    ret = ret+"\n";
+	    //for the next row, I have to add \n and subtract 1 from r
+	    if (r>1)//this if statement removes the extra /n at end of the group of stars (not necessary for the solution) 
+		ret = ret+"\n";
 	    r = r-1;
 	}
 	return ret; 
     }
 }
 
-//one style: keep the original parameters, r and c, throughout the entire fxn. If you need to use r or c, make copies of them and work with the copies instead
+//one style of coding: keep the original parameters, r and c, throughout the entire fxn. If you need to use r or c, make copies of them and work with the copies instead

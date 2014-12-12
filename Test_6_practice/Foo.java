4525a4060201c8a14235b9c2ec2dba75afade4d4
leoncheng57 <57leonardo@gmail.com>
2014-12-11 19:49:30 -0500

    L

--- Test_6_practice/Foo.java
+++ Test_6_practice/Foo.java
@@ -16,28 +16,9 @@ public class Foo{
     }
 
 
-    public static String scrambleWord(String word){
-	String result = "";
-	for (int i=0;i<word.length()-1;i++){
-	    if (word.substring(i,i+1).equals("A") && !(word.substring(i+1,i+2).equals("A"))){
-		result = result + word.substring(i-1,i+2)+word.substring(i,i+1);
-		i+=2;
-	    }
-	    else{
-		result = result + word.substring(i,i+1);
-	    }
-	}
-	result= result+word.substring(word.length()-1);
-	return result;
-    }
-
-
     //MAIN
     public static void main(String[] args){
 	Foo f = new Foo();
-	System.out.println(f.scrambleWord("AARDVARK"));
-	System.out.println(f.scrambleWord("AARDVARKAA"));
-	System.out.println(f.scrambleWord("AARDVARK"));
 
     }
 }
